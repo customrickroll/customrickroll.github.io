76bb9b8c04fb5bcd63fcd7c3cf69066c99ed81f7
Anonymous <>
2021-05-03 16:17:47 -0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,2 @@
+var blockDiv = $(document.createElement('div')).attr('id', 'blockDiv').height('100%').width('100%').css({'z-index':'3333', 'position' : 'absolute', 'top': '0', 'left':'0'});
+$('body').append(blockDiv);
