e5ea0a083fbf084bb9563c189895e9de924f7950
Anonymous <>
2021-05-03 20:46:32 -0700

    Update

---
+++ js/player.js
@@ -0,0 +1,31 @@
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '405',
+        width: '720',
+        videoId: 'QtBDL8EiNZo',
+        playerVars: { 'autoplay': 1, 'controls': 0 },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+var done = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        done = true;
+    }
+}
