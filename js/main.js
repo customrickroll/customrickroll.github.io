eac9470b2334635a238f03a7becd50e9d1d691c4
Anonymous <>
2021-05-03 22:28:42 -0700

    Fix pausing

--- js/main.js
+++ js/main.js
@@ -1,2 +1,4 @@
-var blockDiv = $(document.createElement('div')).attr('id', 'blockDiv').height('100%').width('100%').css({'z-index':'3333', 'position' : 'absolute', 'top': '0', 'left':'0'});
-$('body').append(blockDiv);
+document.addEventListener("click", e => {
+    e.stopPropagation();
+    e.preventDefault();
+}, true);
